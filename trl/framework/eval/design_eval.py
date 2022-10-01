c4e2df6cebd9ab8f541349ddb5426faee0b81fa7
Theodore Galanos <teddian@gmail.com>
2022-10-01 16:11:45 +0800

    Fixed evaluation and added examples / test cases

--- trl/framework/eval/design_eval.py
+++ trl/framework/eval/design_eval.py
@@ -4,20 +4,19 @@ import re
 regex = re.compile(".*?\((.*?)\)")
 
 
-def eval_function(prompt, samples, prompt_type):
-    results = []
+def eval_function(prompts, samples, prompt_types):
+    #results = []
     semantic_accuracy = []
+    type_reward = []
     # assuming a batch of generations sampled from the model
-    for layout in samples:
+    for layout, prompt, prompt_type in zip(prompts, samples, prompt_types):
         try:
             if(len(layout.split('[layout]')) > 1):
                 layout = layout.split('[layout]')[1].split('[User prompt]')[0].split(', ')
             else:
                 layout = layout.split('[Layout]')[1].split('[User prompt]')[0].split(', ')
-            spaces = [txt.split(':')[0].replace('oms', 'om') for txt in layout 
-                      if(type(get_value(housegan_labels, txt.split(':')[0].replace('oms', 'om'))) == int)]
-            space_ids = [get_value(housegan_labels, space) for space in spaces 
-                             if(type(get_value(housegan_labels, space)) == int)]
+            spaces = [re.sub(r'\d+', '', txt.split(':')[0]).lstrip() for txt in layout]
+            space_ids = [get_value(housegan_labels, space) for space in spaces]
             coordinates = [txt.split(':')[1] for txt in layout if len(txt.split(':')) > 1]
             coordinates = [re.findall(regex, coord) for coord in coordinates]
             coordinates = [x for x in coordinates if x != []]
@@ -43,34 +42,47 @@ def eval_function(prompt, samples, prompt_type):
             desc.extend(list(set(flatten(loc_desc))))
             desc = [re.sub('_', ' ', d) for d in desc]
             semantic_accuracy.append(prompt in desc)
+
+            if(prompt_type == 'ind_number_prompt'):
+                req_bed = w2n.word_to_num(prompt.split('with ')[1].split(' ')[0])
+                gen_bed = np.where(np.array(spaces) == 'bedroom')[0].shape[0]
+                bed_difference = abs(req_bed - gen_bed)
+                print(req_bed)
+                req_bath = w2n.word_to_num(prompt.split('and ')[1].split(' ')[0])
+                gen_bath = np.where(np.array(spaces) == 'bathroom')[0].shape[0]
+                bath_difference = abs(req_bath - gen_bath)
+                type_reward.append(bed_difference + bath_difference)
+            elif(prompt_type == 'total_number_prompt'):
+                req_rooms = w2n.word_to_num(prompt.split('with ')[1].split(' ')[0])
+                if('corridor' in prompt):
+                    req_rooms +=1
+                gen_rooms = len(spaces)
+                room_difference = -abs(req_rooms - gen_rooms)
+                print(room_difference)
+                type_reward.append(room_difference)
+            elif(prompt_type == 'location_prompt'):
+                try:
+                    req_location = prompt.split('is located in the ')[1].split(' side')[0]
+                    gen_location = [d for d in desc if 'side of the house' in d]
+                    gen_location = [loc for loc in gen_location if req_location in loc]
+                    gen_cell = gen_location[0].split('is located in the ')[1].split(' side')[0]
+                    adj_cells = location_adjacencies[req_location]
+                    if gen_cell == req_location:
+                        type_reward.append(1)
+                    elif gen_cell in adj_cells:
+                        type_reward.append(-1)
+                    else:
+                        type_reward.append(-2)
+                except:
+                    type_reward.append(-999)
+            else:
+                type_reward.append(None)
         except:
             # what type of values do we put when the model fails to create a valid design?
-            semantic_accuracy = -999
-            type_reward = -999
-        if(prompt_type == 'number_prompt'):
-            req_bed = w2n.word_to_num(prompt.split('with ')[1].split(' ')[0])
-            gen_bed = np.where(np.array(spaces) == 'bedroom')[0].shape[0]
-            bed_difference = abs(req_bed - gen_bed)
+            semantic_accuracy.append(-999)
+            type_reward.append(-999)
 
-            req_bath = w2n.word_to_num(prompt.split('and ')[1].split(' ')[0])
-            gen_bath = np.where(np.array(spaces) == 'bathroom')[0].shape[0]
-            bath_difference = abs(req_bath - gen_bath)
-            type_reward.append(bed_difference + bath_difference)
-        elif( prompt == 'location_prompt'):
-            try:
-                req_location = prompt.split('is located in the ')[1].split(' side')[0]
-                gen_location = [d for d in desc if 'side of the house' in d]
-                gen_location = [loc for loc in gen_location if req_location in loc]
-                gen_cell = gen_location[0].split('is located in the ')[1].split(' side')[0]
-                adj_cells = location_adjacencies[req_location]
-                if gen_cell == req_location:
-                    type_reward = 1
-                elif gen_cell in adj_cells:
-                    type_reward = -1
-                else:
-                    type_reward = -2
-            except:
-                type_reward.append(-999)
-        results.append(semantic_accuracy, type_reward)
+
+    results = {'semantic_accuracy': semantic_accuracy, 'reward': type_reward}
+    #results.append((semantic_accuracy, type_reward))
     return results
-        
